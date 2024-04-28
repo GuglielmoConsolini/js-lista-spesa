862d4f2ee92ceccef95c4d6b94cb2e208899c24d
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-28 18:52:08 +0200

    M1 ciclo for lista spesa

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let listaSpesa = ["latte","uova","pasta","passata di pomodoro","frutta","verdura"];
+console.log(listaSpesa);
+
+for (let i = 0; i < listaSpesa.length; i++) {
+   let elementiLista = listaSpesa[i];
+
+   console.log(elementiLista,i);
+}
