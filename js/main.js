bf7418d1a50c62b6d2099c5d6009a9972c3f03f7
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-28 19:00:11 +0200

    M4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 let listaSpesa = ["latte","uova","pasta","passata di pomodoro","frutta","verdura"];
 console.log(listaSpesa);
 
-const listaHtml = document
+const listaHtml = document.querySelector("ul");
 
 let i = 0;
 while ( i < listaSpesa.length) {
    let elementiLista = listaSpesa[i];
    console.log(elementiLista,i);
    i++;
+
+   listaHtml.innerHTML += `<li>${elementiLista}</li>`;
 }
